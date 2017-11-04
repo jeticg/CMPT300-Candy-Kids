3351611eab6f8074607a2a75b8ad3f781aa670b7
Jetic Gu <jetic@me.com>
2017-11-04 11:22:11 -0700

    Fixing compile bugs on linux

--- src/candykids.c
+++ src/candykids.c
@@ -16,35 +16,35 @@
 
 
 int main(int argc, char *argv[]) {
-    int numFac = 0;
-    int numKid = 0;
-    int numSec = 0;
-
+    /*
+        1. Extract arguments
+    */
     if( argc != 4 ) {
         printf("Usage: ./candykids <#factories> <#kids> <#seconds>\n");
         return 0;
-    } else {
-        // Check if all input meets requirements
-        for (int i=1; i<argc; i++)
-            if (isNumber(argv[i]) == 0) {
-                printf("Error, arguments must be numbers\n\n");
-                printf("Usage: ./candykids <#factories> <#kids> <#seconds>\n");
-                return 0;
-            }
-
-        // Process arguments
-        numFac = convertToNumber(argv[1]);
-        numKid = convertToNumber(argv[2]);
-        numSec = convertToNumber(argv[3]);
-        #ifdef DEBUG
-        printf("--DEBUG: numFac = %d\n", numFac);
-        printf("--DEBUG: numKid = %d\n", numKid);
-        printf("--DEBUG: numSec = %d\n", numSec);
-        #endif
     }
-    // 1. Extract arguments
-    // 2. Initialize modules
-    // 3. Launch candy-factory threads // 4. Launch kid threads
+    // Check if all input meets requirements
+    for (int i=1; i<argc; i++)
+        if (isNumber(argv[i]) == 0) {
+            printf("Error, arguments must be numbers\n\n");
+            printf("Usage: ./candykids <#factories> <#kids> <#seconds>\n");
+            return 0;
+        }
+
+    // Process arguments
+    int numFac = convertToNumber(argv[1]);
+    int numKid = convertToNumber(argv[2]);
+    int numSec = convertToNumber(argv[3]);
+    #ifdef DEBUG
+    printf("--DEBUG: numFac = %d\n", numFac);
+    printf("--DEBUG: numKid = %d\n", numKid);
+    printf("--DEBUG: numSec = %d\n", numSec);
+    #endif
+
+
+    /* 2. Initialize modules */
+    // 3. Launch candy-factory threads
+    // 4. Launch kid threads
     // 5. Wait for requested time
     // 6. Stop candy-factory threads
     // 7. Wait until no more candy
